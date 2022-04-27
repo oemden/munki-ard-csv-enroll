72089c3bb3065598cee613a94115ec57ceda6c3c
localadminoem_mobiloem <oem@mac.com>
2022-04-27 17:41:46 +0200

    Removing error reporting in index.php

--- enrolltothejungle/index.php
+++ enrolltothejungle/index.php
@@ -1,14 +1,15 @@
 <?PHP
-// Version=1.2
+error_reporting (E_ALL ^ E_NOTICE);
+// Version=1.2.1
   $hostdir = $_POST['hostdir'];
   $version = $_POST['version'];
   if(!empty($_FILES['uploaded_file']))
   {
    echo  PHP_EOL . "Enroll To The Jungle - version: " . $version . PHP_EOL ;
-   $manifets_path = '../manifests/' . $hostdir . '/' ; 
+   $manifets_path = '../manifests/' . $hostdir . '/' ;
     $path = $manifets_path . basename( $_FILES['uploaded_file']['name']);
     if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
-      echo "The manifest ".  basename( $_FILES['uploaded_file']['name']). 
+      echo "The manifest ".  basename( $_FILES['uploaded_file']['name']).
       " has been uploaded in munki repo: " . PHP_EOL  . 'manifests/' . $hostdir . " directory" . PHP_EOL ;
     } else {
         echo "There was an error uploading the manifest, please try again!"  . PHP_EOL ;
